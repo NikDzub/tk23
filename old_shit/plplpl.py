3e38b56fd9fd3b0951ffc19fe1b45df4f75b5f86
NikDzub <87159434+NikDzub@users.noreply.github.com>
2023-11-21 23:51:13 +0200

    004

---
+++ old_shit/plplpl.py
@@ -0,0 +1,161 @@
+import asyncio
+from playwright.async_api import async_playwright
+from ppadb.client import Client as AdbClient
+import os
+import shutil
+import json
+
+
+context_dir = "contexts/firefox_01"
+context_dir = os.path.join(os.getcwd(), context_dir)
+
+try:
+    shutil.rmtree(f"{context_dir}/sessionstore-backups")
+    os.remove(f"{context_dir}/sessionCheckpoints.json")
+    os.remove(f"{context_dir}/sessionstore.jsonlz4")
+except Exception as error:
+    pass
+    # print(error)
+
+
+client = AdbClient(host="127.0.0.1", port=5037)
+app_name = "com.zhiliaoapp.musically"
+devices = client.devices()
+
+if len(devices) == 0:
+    print("No devices")
+    quit(0)
+
+
+async def get_output(device):
+    await asyncio.sleep(1)
+    output = device.shell(f"dumpsys activity {app_name}")
+    index_start = output.rindex("Added Fragments:")
+    index_end = output.rindex("}")
+    return output[index_start:index_end]
+
+
+users_db = []
+
+
+async def filter_response(response):
+    global users_db
+    if "/api/user/list/?" in response.url:
+        response = await response.json()
+        users = response["userList"]
+        for user in users:
+            if (
+                (user["user"]["privateAccount"]) != "false"
+                and (user["stats"]["videoCount"]) > 2
+                and user["user"]["uniqueId"] not in users_db
+            ):
+                users_db.append(user["user"]["uniqueId"])
+
+
+async def open_app(device):
+    open_app = f"monkey -p {app_name} 1"
+    device.shell(open_app)
+    # await asyncio.sleep(2)
+
+
+async def verify_load_app(device):
+    for device in devices:
+        await open_app(device)
+        while True:
+            try:
+                output = await get_output(device)
+                if "MainPageFragment" in output:
+                    print("App Loaded")
+                    await asyncio.sleep(1)
+                    break
+            except Exception as error:
+                pass
+                # print(error)
+
+
+def split_list(alist, wanted_parts):
+    length = len(alist)
+    return [
+        alist[i * length // wanted_parts : (i + 1) * length // wanted_parts]
+        for i in range(wanted_parts)
+    ]
+
+
+async def main():
+    global users_db
+
+    async with async_playwright() as p:
+        context = await p.firefox.launch_persistent_context(
+            user_data_dir=context_dir, headless=False
+        )
+        page = context.pages[0]
+        await page.goto("https://www.tiktok.com/@nickiminaj", wait_until="load")
+        await page.wait_for_timeout(1000)
+
+        await page.click('span[data-e2e="followers"]')
+        page.on("response", filter_response)
+
+        while len(users_db) < 50:
+            followers = await page.query_selector_all('p[class*="UniqueId"]')
+            try:
+                await followers[len(followers) - 1].scroll_into_view_if_needed()
+            except Exception as error:
+                pass
+                # print(error)
+
+        users_db = users_db[10:]
+        users_db = split_list(users_db, wanted_parts=len(devices))
+
+        await page.close()
+
+        # open each app sync
+        for device in devices:
+            await open_app(device)
+            await verify_load_app(device)
+
+        async def blah(device, index):
+            users_chunk = users_db[index]
+            print(index)
+            print(users_chunk)
+
+            page = await context.new_page()
+
+            for users in users_chunk:
+                videos_db = []
+                print(users)
+
+                # async def book_mark(device, url):
+                #     device.shell(
+                #         f"am start -W -a android.intent.action.VIEW -d {url} {app_name}"
+                #     )
+                #     await asyncio.sleep(2)
+                #     device.shell(f"input tap 440 290")
+                #     await asyncio.sleep(1)
+
+                # try:
+                #     await page.goto(f"https://tiktok.com/@{user}", wait_until="load")
+                #     await page.wait_for_selector(f"a[href*='{user}/video']")
+
+                #     videos_selector = await page.query_selector_all(
+                #         f"a[href*='{user}/video']"
+                #     )
+
+                #     for video in videos_selector:
+                #         url = await video.get_attribute("href")
+                #         if url not in videos_db and len(videos_db) < 2:
+                #             videos_db.append(url)
+
+                #     for url in videos_db:
+                #         await book_mark(device, url)
+
+                # except Exception as error:
+                #     pass
+                #     # print(error)
+
+        asyncio.gather(
+            *[blah(device, index) for index, device in enumerate(devices)],
+            return_exceptions=True,
+        )
+
+
+asyncio.run(main())
---
+++ p2.py
@@ -0,0 +1,92 @@
+import asyncio
+from ppadb.client import Client as AdbClient
+import os
+import shutil
+import json
+
+
+client = AdbClient(host="127.0.0.1", port=5037)
+app_name = "com.zhiliaoapp.musically"
+devices = client.devices()
+
+if len(devices) == 0:
+    print("No devices")
+    quit(0)
+
+
+async def get_output(device):
+    await asyncio.sleep(1)
+    output = device.shell(f"dumpsys activity {app_name}")
+    index_start = output.rindex("Added Fragments:")
+    index_end = output.rindex("}")
+    return output[index_start:index_end]
+
+
+def split_list(alist, wanted_parts):
+    length = len(alist)
+    return [
+        alist[i * length // wanted_parts : (i + 1) * length // wanted_parts]
+        for i in range(wanted_parts)
+    ]
+
+
+async def open_app(device):
+    open_app = f"monkey -p {app_name} 1"
+    device.shell(open_app)
+    # await asyncio.sleep(2)
+
+
+async def verify_load_app(device):
+    for device in devices:
+        await open_app(device)
+        while True:
+            try:
+                output = await get_output(device)
+                if "MainPageFragment" in output:
+                    # print("App Loaded")
+                    await asyncio.sleep(1)
+                    break
+            except Exception as error:
+                pass
+                # print(error)
+
+
+new_videos = []
+with open("new_videos.txt", "r", encoding="utf-8") as f:
+    new_videos = f.read().splitlines()
+new_users = []
+with open("new_users.txt", "r", encoding="utf-8") as f:
+    new_users = f.read().splitlines()
+
+new_videos = split_list(new_videos, len(devices))
+
+
+async def start_apps():
+    for device in devices:
+        await verify_load_app(device)
+
+
+async def loop_vids(index, videos_chunk):
+    for vid in videos_chunk:
+        devices[index].shell(
+            f"am start -W -a android.intent.action.VIEW -d {vid} {app_name}"
+        )
+        await asyncio.sleep(2)
+        devices[index].shell(f"input keyevent 62")
+        devices[index].shell(f"input tap 440 290")
+        await asyncio.sleep(1)
+
+
+async def main():
+    await start_apps()
+    await asyncio.gather(
+        *[
+            loop_vids(index, videos_chunk)
+            for index, videos_chunk in enumerate(new_videos)
+        ]
+    )
+    with open("used_users", "a") as f:
+        f.write("\n".join(new_users))
+
+
+asyncio.run(main())
